55873826a015f2bc5b84e796b943c2925936423a
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2012-12-16 21:59:50 +0100

    Re-structuring source

--- lib/madeleine/rack/middleware.rb
+++ lib/madeleine/rack/middleware.rb
@@ -7,55 +7,54 @@ require 'madeleine/rack/errors_proxy'
 
 module Madeleine
   module Rack
-  end
-end
+    class Middleware
+      def self.global_app
+        @@global_app
+      end
 
-class Madeleine::Rack::Middleware
-  def self.global_app
-    @@global_app
-  end
+      def self.global_app=(app)
+        @@global_app = app
+      end
 
-  def self.global_app=(app)
-    @@global_app = app
-  end
+      class LoggedRequest
 
-  class LoggedRequest
+        def initialize(env)
+          @env = env.dup
 
-    def initialize(env)
-      @env = env.dup
+          @env['rack.logger'] = Madeleine::Rack::LoggerProxy.new(env['rack.logger'])
+          @env['rack.input'] = Madeleine::Rack::InputProxy.new(env['rack.input'])
+          @env['rack.errors'] = Madeleine::Rack::ErrorsProxy.new(env['rack.errors'])
+        end
 
-      @env['rack.logger'] = Madeleine::Rack::LoggerProxy.new(env['rack.logger'])
-      @env['rack.input'] = Madeleine::Rack::InputProxy.new(env['rack.input'])
-      @env['rack.errors'] = Madeleine::Rack::ErrorsProxy.new(env['rack.errors'])
+        def execute(system)
+          puts "execute(#{system})"
+
+          Thread.current[:_madeleine_system] = system
+          Madeleine::Rack::Middleware.global_app.call(@env)
+        end
+
+        def marshal_dump
+          result = @env.dup
+          result.delete 'async.callback' # Used by Thin
+          result
+        end
+
+        def marshal_load(obj)
+          @env = obj
+        end
+      end
+
+      def initialize(app, storage_directory)
+        Madeleine::Rack::Middleware.global_app = app
+        @madeleine = Madeleine::SnapshotMadeleine.new(storage_directory) {
+          Hash.new
+        }
+      end
+
+      def call(env)
+        transaction = LoggedRequest.new(env)
+        @madeleine.execute_command(transaction)
+      end
     end
-
-    def execute(system)
-      puts "execute(#{system})"
-
-      Thread.current[:_madeleine_system] = system
-      Madeleine::Rack::Middleware.global_app.call(@env)
-    end
-
-    def marshal_dump
-      result = @env.dup
-      result.delete 'async.callback' # Used by Thin
-      result
-    end
-
-    def marshal_load(obj)
-      @env = obj
-    end
-  end
-
-  def initialize(app, storage_directory)
-    Madeleine::Rack::Middleware.global_app = app
-    @madeleine = Madeleine::SnapshotMadeleine.new(storage_directory) {
-      Hash.new
-    }
-  end
-
-  def call(env)
-    transaction = LoggedRequest.new(env)
-    @madeleine.execute_command(transaction)
   end
 end
